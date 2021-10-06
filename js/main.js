0a7da97ffd57afeca49b1c38843458d49af96796
Bhikhi <bhikhi19gc@gmail.com>
2021-10-06 17:52:25 +0530

    removing smooth scroll

--- js/main.js
+++ js/main.js
@@ -52,14 +52,15 @@
 
 /* 
    Sticky Nav
-   ========================================================================== 
+   ========================================================================== */
     $(window).on('scroll', function() {
+      /**
         if ($(window).scrollTop() > 200) {
             $('.header-top-area').addClass('menu-bg');
         } else {
             $('.header-top-area').removeClass('menu-bg');
-        }
-        /**Adjust team - founder position on scroll
+        }**/
+        /**Adjust team - founder position on scroll**/
         if($(window).scrollTop() > 1200)
             $('#teamFounder').css('position', 'absolute');
         if($(window).scrollTop() < 1200)
@@ -68,7 +69,7 @@
 
   /* 
    SMOOTH SCROLL
-   ========================================================================== */
+   ========================================================================== 
     var scrollAnimationTime = 1200,
         scrollAnimation = 'easeInOutExpo';
 
